ba354484293f7931018139c6f6957e154c07ce1e
Oscar Mancilla <oscarmancilla227@gmail.com>
2024-03-14 20:13:58 -0600

    se metio la pagina web

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 import  express  from "express";
 const app = express();
 
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+app.use(express.static("public"));
+
 
 app.get("/test", (req, res) => {
 
@@ -11,6 +17,12 @@ app.get("/test", (req, res) => {
 app.get("/regino", (req, res) => {
     res.send("Chinga tu madre regino");
     res.end();
+})
+
+
+app.get("/generic", (req, res) => {
+
+    res.sendFile(__dirname + "/public/generic.html")
 })
 
 app.listen(8080);
